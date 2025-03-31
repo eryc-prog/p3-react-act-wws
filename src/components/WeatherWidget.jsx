600054bed49dba0633255fa19cd90f07cdb030fe
Eric Encarnacion <encarnacionerc@gmail.com>
2025-03-31 16:26:01 +0800

    fix: delete some codes

--- src/components/WeatherWidget.jsx
+++ src/components/WeatherWidget.jsx
@@ -65,24 +65,6 @@ function WeatherWidget() {
     return <p>Loading...</p>;
   }
 
-  // Function to check API configuration //TRIALL//
-  function checkConfig() {
-    if (API_KEY) {
-      console.error("API key is not available");
-      alert("Application is not properly configured.");
-      return false;
-    }
-    return true;
-  }
-
-  // Initialize the app
-  document.addEventListener("DOMContentLoaded", async () => {
-    await getAPIKey(); // Fetch API key when app loads
-    if (checkConfig()) {
-      fetchLocationWeather();
-    }
-  });
-
   return (
     <div
       className="bg-center p-4 rounded-lg shadow-md ml-4 mr-4 text-white"
