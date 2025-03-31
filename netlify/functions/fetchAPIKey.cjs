3fa76edb033e8df1cb58f9a85611b97b2f24a717
Eric Encarnacion <encarnacionerc@gmail.com>
2025-03-31 16:10:14 +0800

    fix: add some variable to netlify functions

--- netlify/functions/fetchAPIKey.cjs
+++ netlify/functions/fetchAPIKey.cjs
@@ -1,6 +1,6 @@
 exports.handler = async function () {
   return {
     statusCode: 200,
-    body: JSON.stringify({ apiKey: process.env.API_KEY }), // Ensure API_KEY is set in Netlify environment variables
+    body: JSON.stringify({ apiKey: process.env.REACT_APP_API_KEY }), // Ensure API_KEY is set in Netlify environment variables
   };
 };
