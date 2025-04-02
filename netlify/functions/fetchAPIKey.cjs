9634b8d6be3648777c06923957d9759dd482271c
Eric Encarnacion <encarnacionerc@gmail.com>
2025-04-02 17:53:30 +0800

    fix: update fetchAPIKey.js file

--- netlify/functions/fetchAPIKey.cjs
+++
@@ -1,7 +0,0 @@
-exports.handler = async function (event, context) {
-  const apiKey = process.env.API_KEY;
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "This is from your netlify functions!" }), // Message
-  };
-};
