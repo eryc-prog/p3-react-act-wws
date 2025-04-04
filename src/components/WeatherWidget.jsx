558fcc3a7b11f43be703dc79a8f798ec9eb49dea
Eric Encarnacion <encarnacionerc@gmail.com>
2025-04-04 14:43:02 +0800

    fix: re modify the api config

--- src/components/WeatherWidget.jsx
+++ src/components/WeatherWidget.jsx
@@ -3,6 +3,7 @@ import capti from "/src/assets/capti.jpg";
 import React from "react";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = `https://api.weatherapi.com/v1`;
 
 function WeatherWidget({ isSidebarCollapsed }) {
   const [weather, SetWeather] = useState(0);
@@ -12,14 +13,8 @@ function WeatherWidget({ isSidebarCollapsed }) {
   useEffect(() => {
     const fetchWeather = async (latitude, longitude) => {
       try {
-        const API_KEY = process.env.REACT_APP_API_KEY;
-        console.log("API Key:", process.env.REACT_APP_API_KEY);
-        if (!API_KEY) {
-          throw new Error("API Key is not defined in environment variables");
-        }
-
         const response = await fetch(
-          `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${latitude},${longitude}`
+          `${BASE_URL}?key=${API_KEY}&q=${latitude},${longitude}`
         );
         if (!response.ok) {
           throw new Error("Failed to fetch weather data");
